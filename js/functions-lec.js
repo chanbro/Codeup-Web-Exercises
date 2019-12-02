f659a6efcfffca6b46406867542b9c7d5354fe36
Chaney <52712847+chanbro@users.noreply.github.com>
2019-12-02 15:05:40 -0600

    function lecture notes

---
+++ js/functions-lec.js
@@ -0,0 +1,12 @@
+"use strict";
+//Use if program is run by web browser
+/*
+
+Important terms: functions, procedures, methods, sub-routines (less common)
+
+"Functions are first class citizens"
+Anything 5-50 lines of code should be encapsulated in function
+You can put var inside a function!!
+Run/call/evoke/execute function = make function appear in console/on page; you can set a function but it won't be applied until it is called
+
+ */
