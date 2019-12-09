6638cfeb764b01b9f9839e29f9a303ef06d56285
Chaney <52712847+chanbro@users.noreply.github.com>
2019-12-09 13:58:45 -0600

    added arrays lecture and exercise

---
+++ js/arrays-lecture.js
@@ -0,0 +1,22 @@
+// var pet1 = "Toby";
+// var pet2 = "Princess";
+// var pet3 = "Samson";
+// var pet4 = "Jill";
+// var pet5 = "Bubbles";
+// var pet6 = "Malu";
+
+// "Code stench": no relationship between variables
+
+var pets = [];
+// or var pets = ["Toby", "Princess", "Samson", "Jill", "Bubbles", "Malu"];
+
+pets[0] = "Toby";
+pets[1] = "Princess";
+pets[2] = "Samson";
+pets[3] = "Jill";
+pets[4] = "Bubbles";
+pets[5] = "Malu";
+
+console.log(pets);
+//output: (6) ["Toby", "Princess", "Samson", "Jill", "Bubbles", "Malu"]
+// Assigning index to each variable to be included in array
