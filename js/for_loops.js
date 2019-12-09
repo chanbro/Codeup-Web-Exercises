ec6377be39ab9386ca6dd965df99af0e2bd764cd
Chaney <52712847+chanbro@users.noreply.github.com>
2019-12-09 13:42:16 -0600

    added loop exercises

---
+++ js/for_loops.js
@@ -0,0 +1,112 @@
+"use strict";
+
+// 7 x 1 = 7
+// 7 x 2 = 14
+// 7 x 3 = 21
+// 7 x 4 = 28
+// 7 x 5 = 35
+// 7 x 6 = 42
+// 7 x 7 = 49
+// 7 x 8 = 56
+// 7 x 9 = 63
+// 7 x 10 = 70
+
+
+// function showMultiplicationTable(numberInput){
+//
+// 	for (var i = 1; i <= 10; i++){
+// 		var answer = numberInput * i;
+//
+// 		console.log(numberInput + " x " + i + " = " + answer)
+// 	}
+//
+// }
+
+// showMultiplicationTable(7);
+
+
+
+
+
+
+// for (var i = 1; i <= 10; i++){
+// 	var randomNumber = Math.floor(Math.random() * 180) + 20;
+//
+// 	if(randomNumber % 2 === 0){
+// 		console.log(randomNumber + " is even");
+// 	} else {
+// 		console.log(randomNumber + " is odd");
+// 	}
+// }
+
+
+
+
+// 1
+// 22
+// 333
+// 4444
+// 55555
+// 666666
+// 7777777
+// 88888888
+// 999999999
+
+//
+// for(var i = 1; i < 10; i++){
+// 	console.log(i.toString().repeat(i));
+// }
+//
+//
+//
+// for (var outer = 1; outer <=9; outer++) {
+//
+// 	var output = '';
+//
+// 	for (var inner = 1; inner<=outer; inner++) {
+// 		output += outer;
+// 	}
+//
+// 	console.log(output);
+// }
+
+
+
+
+// for(var i = 100; i >= 5; i-=5){
+//     console.log(i);
+// }
+//
+//
+//
+//
+// for(var i = 100; i >=5; i--){
+//
+//     if(i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
