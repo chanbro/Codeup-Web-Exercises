505374475ec354b13c7a2690c2db48c7497e884f
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-06 11:42:36 -0600

    completed jQuery exercise 2

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -6,14 +6,30 @@ $(document).ready(function() {
     // alert("complete");
     // console.log($);
 
-    $('#important').css("background-color", "cyan");
+    // $('#important').css("background-color", "cyan");
+    //
+    // $('li').addClass("purple");
+    //
+    // $('.purple').css('color','purple');
+    //
+    // $("#button").click(function () {
+    //     alert("You clicked the button")
+    // })
 
-    $('li').addClass("purple");
+    $('a').addClass('side-nav');
 
-    $('.purple').css('color','purple');
+    $('.side-nav').click(function () {
+        alert("Scrolling")
+    });
 
-    $("#button").click(function () {
-        alert("You clicked the button")
-    })
+    $('ol.projects > li').addClass('codeup');
+    $('ol').children('li').eq(2).removeClass('codeup');
+    $('.codeup').css('border', '1px solid red');
+    $('ol').children('li').eq(2).attr('id','codeup')
 
+    $('li').css('font-size', '20px')
+    $('h1, p, li').css('background-color', 'pink')
+
+    var contents = $('h1').html();
+    alert(contents);
 });
