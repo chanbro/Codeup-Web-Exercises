5aa10fe53f797d0d2e163e8cfe01d9115d386958
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-06 10:49:36 -0600

    completed first jQuery exercise

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -1,7 +1,19 @@
 "use strict";
 
+//function passed to the .ready jQuery method; document object passed through jQuery (doesn't interact with DOM until page is loaded)
 $(document).ready(function() {
 
-    alert("complete");
+    // alert("complete");
+    // console.log($);
+
+    $('#important').css("background-color", "cyan");
+
+    $('li').addClass("purple");
+
+    $('.purple').css('color','purple');
+
+    $("#button").click(function () {
+        alert("You clicked the button")
+    })
 
 });
