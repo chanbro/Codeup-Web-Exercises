c620c8fab297c91fb1b29863a2ed1508a130fec0
Chaney <52712847+chanbro@users.noreply.github.com>
2020-01-06 16:37:22 -0600

    completed jquery exercises

--- js/my-custom-javascript.js
+++ js/my-custom-javascript.js
@@ -15,21 +15,42 @@ $(document).ready(function() {
     // $("#button").click(function () {
     //     alert("You clicked the button")
     // })
+    //
+    // $('a').addClass('side-nav');
+    //
+    // $('.side-nav').click(function () {
+    //     alert("Scrolling")
+    // });
+    //
+    // $(document).ready(function () {
+    //     alert($('#button').html())
+    // });
+    //
+    // $('ol.projects > li').addClass('codeup');
+    // $('ol').children('li').eq(2).removeClass('codeup');
+    // $('.codeup').css('border', '1px solid red');
+    // $('ol').children('li').eq(2).attr('id','codeup');
+    //
+    // $('li').css('font-size', '20px');
+    // $('h1, p, li').css('background-color', 'pink');
+    // alert($('h1').html());
+    // var contents = $('h1').html();
+    // alert(contents);
 
-    $('a').addClass('side-nav');
+    $('h1').click(function(){
+        $(this).css('background-color','green')
+    })
 
-    $('.side-nav').click(function () {
-        alert("Scrolling")
-    });
+    $('p').dblclick(function () {
+        $(this).css('font-size', '18px')
+    })
 
-    $('ol.projects > li').addClass('codeup');
-    $('ol').children('li').eq(2).removeClass('codeup');
-    $('.codeup').css('border', '1px solid red');
-    $('ol').children('li').eq(2).attr('id','codeup')
-
-    $('li').css('font-size', '20px')
-    $('h1, p, li').css('background-color', 'pink')
-
-    var contents = $('h1').html();
-    alert(contents);
+    $('li').hover(
+        function () {
+            $('li').css('color', 'red')
+        },
+        function () {
+            $('li').css('color', 'black')
+        }
+    )
 });
